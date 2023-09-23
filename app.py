869c1ff5fa3c2ff41664c30c03866bbcd1381c27
jiohh <dkswlgh99@naver.com>
2023-09-23 16:10:19 +0900

    init

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
-import config
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!' + config.test
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
