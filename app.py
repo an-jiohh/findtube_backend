9f397be8cdc393c527db576df214c63da09a7d8a
jiohh <dkswlgh99@naver.com>
2023-09-23 16:07:43 +0900

    init

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+import config
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello World!' + config.test
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True, port=5655)
